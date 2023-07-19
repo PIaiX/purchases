8e8896cff88b1b983b1a7e965389984551c4469e
ngf93 <nabiullinaguzel93@gmail.com>
2023-07-19 16:18:13 +0300

    fix home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,14 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import MainSlider from '../components/MainSlider';
-import OfferCard from '../components/OfferCard';
-import Chat from '../components/chat/Chat';
 import BlogSection from '../components/BlogSection';
 
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Scrollbar, Mousewheel } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/scrollbar';
 
-import useIsMobile from '../hooks/isMobile';
 import CatalogSection from '../components/CatalogSection';
 
 const Home = () => {
-  const isMobileLG = useIsMobile('1109px');
-
   return (
     <main>
       <Container>
