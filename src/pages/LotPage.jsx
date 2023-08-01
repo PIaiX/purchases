b297599e91e67a77a06186b07931df91e6811088
ngf93 <nabiullinaguzel93@gmail.com>
2023-08-01 18:11:16 +0300

    lot page full

--- src/pages/LotPage.jsx
+++ src/pages/LotPage.jsx
@@ -20,16 +20,18 @@ const LotPage = () => {
                 </Link>
 
                 <Row>
-                    <Col md={8}>
+                    <Col xs={12} lg={8}>
                         <div className="lot-page-box lot-page-grid mb-4">
                             <div className="game">
                                 <img src="imgs/img5.jpg" alt="AFK Arena" />
-                                <h6>AFK Arena</h6>
+                                <h6 className='mt-2'>AFK Arena</h6>
                             </div>
 
                             <div className='info'>
-                                <span className='tag-gray me-3'>Аккаунты</span>
-                                <span className='tag-green me-3'>RU/EU</span>
+                                <div className='d-flex align-items-center'>
+                                    <span className='tag-gray me-3'>Аккаунты</span>
+                                    <span className='tag-green me-3'>RU/EU</span>
+                                </div>
                                 <div className='d-flex align-items-center'>
                                     <span>Сервер</span>
                                     <span className='fs-09 pale-blue mx-2'>●</span>
@@ -37,7 +39,9 @@ const LotPage = () => {
                                 </div>
                             </div>
 
-                            <p className='title'>Тяж, Лайт, Маг Сэт Ада Пустые, Наборы (сеты), R</p>
+                            <div className='title'>
+                                <p>Тяж, Лайт, Маг Сэт Ада Пустые, Наборы (сеты), R</p>
+                            </div>
 
                             <div className='date'>
                                 <time>14:51</time>
@@ -49,7 +53,7 @@ const LotPage = () => {
 
                             <div className="payment">
                                 <LabeledInput 
-                                    className={"min-250 me-4"}
+                                    className={"min-250 me-md-4"}
                                     type={"select"} 
                                     label={"Выберите способ оплаты"} 
                                     options={[{value:1, text: 'Банковская карта'}, {value:2, text: 'СБП'}, {value:3, text: 'MIR PAY'}]}
@@ -71,122 +75,11 @@ const LotPage = () => {
                                     <span>Очень длинное значение</span>
                                 </li>
                             </ul>
-
-                            {/* <div className="p-3">
-                                <div className='d-flex justify-content-between align-items-start'>
-                                    <div className="d-flex align-items-end">
-                                        
-                                        <div>
-                                            
-                                            
-                                        </div>
-                                    </div>
-                                    
-                                    <div className='light-gray d-flex align-items-center'>
-                                        <time>14:51</time>
-                                        <time className='ms-3'>16/03/2023</time>
-                                        <button type='button' className='d-flex fs-14 ms-3'>
-                                            <PiWarningLight/>
-                                        </button>
-                                    </div>
-                                </div>
-                                <div className="mt-4 d-flex">
-                                    <LabeledInput 
-                                        className={"min-250 me-4"}
-                                        type={"select"} 
-                                        label={"Выберите способ оплаты"} 
-                                        options={[{value:1, text: 'Банковская карта'}, {value:2, text: 'СБП'}, {value:3, text: 'MIR PAY'}]}
-                                    />
-                                    <button type='button' className='btn-1'>Оплатить 186,97 ₽</button>
-                                </div>
-                            </div>
-                            <hr />
-                            <div className="p-3">
-                                <div className="row row-cols-2">
-                                    <div className='fs-09'>
-                                        <p>Какое то продающее описание товара </p>
-                                    </div>
-                                    <div>
-                                        <ul className='specifications'>
-                                            <li>
-                                                <span>Хакатеристика</span>
-                                                <span>Значение</span>
-                                            </li>
-                                            <li>
-                                                <span>Хакатеристика</span>
-                                                <span>Очень длинное значение</span>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div> */}
-                        </div>
-
-                        <div className="lot-page-box mb-4">
-                            <div className="p-3">
-                                <div className='d-flex justify-content-between align-items-start'>
-                                    <div className="d-flex align-items-end">
-                                        <div className="game me-3">
-                                            <img src="imgs/img5.jpg" alt="AFK Arena" />
-                                            <h6>AFK Arena</h6>
-                                        </div>
-                                        <div>
-                                            <div className='d-flex align-items-center mb-2'>
-                                                <span className='tag-gray me-3'>Аккаунты</span>
-                                                <span className='tag-green me-3'>RU/EU</span>
-                                                <div className='d-flex align-items-center'>
-                                                    <span>Сервер</span>
-                                                    <span className='fs-09 pale-blue mx-2'>●</span>
-                                                    <span>Airin + Blackbird</span>
-                                                </div>
-                                            </div>
-                                            <p className='blue'>Тяж, Лайт, Маг Сэт Ада Пустые, Наборы (сеты), R</p>
-                                        </div>
-                                    </div>
-                                    
-                                    <div className='light-gray d-flex align-items-center'>
-                                        <time>14:51</time>
-                                        <time className='ms-3'>16/03/2023</time>
-                                        <button type='button' className='d-flex fs-14 ms-3'>
-                                            <PiWarningLight/>
-                                        </button>
-                                    </div>
-                                </div>
-                                <div className="mt-4 d-flex">
-                                    <LabeledInput 
-                                        className={"min-250 me-4"}
-                                        type={"select"} 
-                                        label={"Выберите способ оплаты"} 
-                                        options={[{value:1, text: 'Банковская карта'}, {value:2, text: 'СБП'}, {value:3, text: 'MIR PAY'}]}
-                                    />
-                                    <button type='button' className='btn-1'>Оплатить 186,97 ₽</button>
-                                </div>
-                            </div>
-                            <hr />
-                            <div className="p-3">
-                                <div className="row row-cols-2">
-                                    <div className='fs-09'>
-                                        <p>Какое то продающее описание товара </p>
-                                    </div>
-                                    <div>
-                                        <ul className='specifications'>
-                                            <li>
-                                                <span>Хакатеристика</span>
-                                                <span>Значение</span>
-                                            </li>
-                                            <li>
-                                                <span>Хакатеристика</span>
-                                                <span>Очень длинное значение</span>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
                         </div>
 
                         <div className="lot-page-box">
-                            <div className="px-3 py-2 d-flex justify-content-between align-items-center">
-                                <div className="seller">
+                            <div className="px-3 py-2 d-sm-flex justify-content-between align-items-center">
+                                <div className="seller w-xs-100">
                                     <img src="imgs/user.jpg" alt="Weatherwax" />
                                     <h3 className='title-font lh-n mb-0'>Weatherwax</h3>
                                     <div className='rating ms-3'>
@@ -194,17 +87,19 @@ const LotPage = () => {
                                         <span>4,5</span>
                                     </div>
                                 </div>
-                                <div className='d-flex align-items-center'>
-                                    <div>
-                                        <p className='fs-09 mb-2'>Сделки</p>
-                                        <p className="fs-15 title-font lh-n">200</p>
-                                    </div>
-                                    <div className='ms-4'>
-                                        <p className='fs-09 mb-2'>Лоты</p>
-                                        <p className="fs-15 title-font lh-n">2266</p>
+                                <div className='mt-3 mt-md-0 d-flex align-items-center justify-content-between w-xs-100'>
+                                    <div className="d-flex">
+                                        <div>
+                                            <p className='fs-09 mb-1 mb-sm-2'>Сделки</p>
+                                            <p className="fs-15 title-font lh-n">200</p>
+                                        </div>
+                                        <div className='ms-4'>
+                                            <p className='fs-09 mb-1 mb-sm-2'>Лоты</p>
+                                            <p className="fs-15 title-font lh-n">2266</p>
+                                        </div>
                                     </div>
                                     <div className='ms-5'>
-                                        <button type='button' className='d-flex gray fs-13 mb-3'><FiShare/></button>
+                                        <button type='button' className='d-flex gray fs-13 mb-2 mb-sm-3'><FiShare/></button>
                                         <button type='button' className='d-flex gray fs-13'><FiAlertTriangle/></button>
                                     </div>
                                 </div>
@@ -217,7 +112,7 @@ const LotPage = () => {
                             <div className="p-3"></div>
                         </div>
                     </Col>
-                    <Col md={4}>
+                    <Col xs={12} lg={4} className='mt-5 mt-lg-0'>
                         <h2 className='fs-15'>Отзывы</h2>
                         <ul className='list-unstyled'>
                             <li className='mb-3'>
