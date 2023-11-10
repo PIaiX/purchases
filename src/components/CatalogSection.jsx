3094e0ed1215692de437aae085e077a3226ff545
Rus <110812530+ManapovFatih@users.noreply.github.com>
2023-11-10 15:14:08 +0300

    alfavit

--- src/components/CatalogSection.jsx
+++ src/components/CatalogSection.jsx
@@ -92,33 +92,9 @@ const CatalogSection = () => {
               <input type="text" placeholder='Поиск' className='p-blue' />
             </form>
             <ul>
-              <li><Link to="/#letter-A" data-to-scrollspy-id="letter-A">A</Link></li>
-              <li><Link to="/#letter-B" data-to-scrollspy-id="letter-B">B</Link></li>
-              <li><Link to="/#letter-C" data-to-scrollspy-id="letter-C">C</Link></li>
-              <li><Link to="/#letter-D" data-to-scrollspy-id="letter-D">D</Link></li>
-              <li><Link to="/#letter-E" data-to-scrollspy-id="letter-E">E</Link></li>
-              <li><Link to="/#letter-F" data-to-scrollspy-id="letter-F">F</Link></li>
-              <li><Link to="/#letter-G" data-to-scrollspy-id="letter-G">G</Link></li>
-              <li><Link to="/#letter-G" data-to-scrollspy-id="letter-G">H</Link></li>
-              <li><Link to="/#letter-I" data-to-scrollspy-id="letter-I">I</Link></li>
-              <li><Link to="/#letter-J" data-to-scrollspy-id="letter-J">J</Link></li>
-              <li><Link to="/#letter-K" data-to-scrollspy-id="letter-K">K</Link></li>
-              <li><Link to="/#letter-L" data-to-scrollspy-id="letter-L">L</Link></li>
-              <li><Link to="/#letter-M" data-to-scrollspy-id="letter-M">M</Link></li>
-              <li><Link to="/#letter-N" data-to-scrollspy-id="letter-N">N</Link></li>
-              <li><Link to="/#letter-O" data-to-scrollspy-id="letter-O">O</Link></li>
-              <li><Link to="/#letter-P" data-to-scrollspy-id="letter-P">P</Link></li>
-              <li><Link to="/#letter-Q" data-to-scrollspy-id="letter-Q">Q</Link></li>
-              <li><Link to="/#letter-R" data-to-scrollspy-id="letter-R">R</Link></li>
-              <li><Link to="/#letter-S" data-to-scrollspy-id="letter-S">S</Link></li>
-              <li><Link to="/#letter-T" data-to-scrollspy-id="letter-T">T</Link></li>
-              <li><Link to="/#letter-U" data-to-scrollspy-id="letter-U">U</Link></li>
-              <li><Link to="/#letter-V" data-to-scrollspy-id="letter-V">V</Link></li>
-              <li><Link to="/#letter-W" data-to-scrollspy-id="letter-W">W</Link></li>
-              <li><Link to="/#letter-X" data-to-scrollspy-id="letter-X">X</Link></li>
-              <li><Link to="/#letter-Y" data-to-scrollspy-id="letter-Y">Y</Link></li>
-              <li><Link to="/#letter-Z" data-to-scrollspy-id="letter-Z">Z</Link></li>
-              <li><Link to="/#letter-ru" data-to-scrollspy-id="letter-ru">А-Я</Link></li>
+              {games.data && games.items && games.data.map((letter) => (
+                <li><Link to={"/#letter-" + letter} data-to-scrollspy-id={"letter-" + letter}>{letter}</Link></li>
+              ))}
             </ul>
             <div ref={cut} id="cut" className={(full) ? 'opened' : ''}><img src={Arrow} alt="arrow" /></div>
           </div>
