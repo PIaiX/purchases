e7e1eb7e946adc3f4bd42e110b1542b70e778a97
Rus <110812530+ManapovFatih@users.noreply.github.com>
2023-12-21 17:41:41 +0300

    1

--- src/components/Operation.jsx
+++ src/components/Operation.jsx
@@ -24,7 +24,7 @@ const Operation = ({ createdAt, orderId, id, status, price, className }) => {
           ))
       }
       <div className="stat">
-        {status ?
+        {status == "ok" ?
           <span className="blue">Исполнено</span>
           : <span className="green">В обработке</span>
 
