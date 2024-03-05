f33489a44ba05079f64091f1957483cc747d27ca
Fatih <110812530+ManapovFatih@users.noreply.github.com>
2024-03-05 18:18:38 +0300

    1

--- src/components/chat/Message.jsx
+++ src/components/chat/Message.jsx
@@ -1,120 +1,130 @@
-import React from 'react';
+import React, { useState } from 'react';
 import moment from "moment";
 import WarningIcon from '../svg/WarningIcon';
 
 import { IoCheckmarkDoneOutline, IoCheckmarkOutline } from "react-icons/io5";
 import { Link } from 'react-router-dom';
 import { getImageURL } from '../../helpers/all';
+import { Modal } from 'react-bootstrap';
 
 
 const Message = ({ my, userId, general, createdAt, media, text, name, admin, user, view, type, status }) => {
   const image = getImageURL({ path: user, type: "user" })
+  const [showShare, setShowShare] = useState(false);
   const time = createdAt
     ? moment(createdAt).format("DD.MM.YYYY kk:mm")
     : moment().format("DD.MM.YYYY kk:mm");
-  return my ? (
-    <div className="chat-window-message-mine">
-      <div className='text'>
-        <div className='gray fs-08 d-flex align-items-center mb-2'>
-          {type == "system" &&
-            <h6 className="name ms-3">Системное сообщение</h6>
+  return (
 
-          }
-          <time className='me-2'>
-            {time}
-          </time>
-          {view ? (
-            <IoCheckmarkDoneOutline className="text-success ms-1" size={15} />
-          ) : (
-            <IoCheckmarkOutline className="ms-1" size={15} />
-          )}
 
-        </div>
-        <div className={type == "system" || !status ? "bubble colored" : "bubble"}>
-          {status ?
-            <>
-              {text && <p>{text}</p>}
-              {media &&
-                <Link to={getImageURL({ path: media, size: "", type: "message" })}>
-                  <img src={getImageURL({ path: media, size: "", type: "message" })} className="bubble-img" />
-                </Link>
+    <>
+      {my ? (
+        <div className="chat-window-message-mine">
+          <div className='text'>
+            <div className='gray fs-08 d-flex align-items-center mb-2'>
+              {type == "system" &&
+                <h6 className="name ms-3 me-2">Системное сообщение</h6>
+
               }
-            </>
-            :
-            <p>Сообщение заблокировано администрацией</p>
-          }
-        </div>
-      </div>
-      {type == "system" ?
-        <img src="/imgs/system.png" alt={name} />
-        :
-        <img src={image} alt={name} />
-      }
-    </div>
+              <time className='me-2'>
+                {time}
+              </time>
+              {view ? (
+                <IoCheckmarkDoneOutline className="text-success ms-1" size={15} />
+              ) : (
+                <IoCheckmarkOutline className="ms-1" size={15} />
+              )}
 
-  ) : (
-    admin ? (
-      <div className="chat-window-message">
-        <img src={image} alt={name} />
-        <div className='text'>
-          <div className='gray fs-08 d-flex align-items-center mb-2'>
-            <h6 className="me-2 fs-12">ADMIN </h6>
-            <time className='me-2'> {time}</time>
-
-          </div>
-          <div className="bubble colored">
-            {text && <p>{text}</p>}
-            {media &&
-              <Link to={getImageURL({ path: media, size: "", type: "message" })} >
-                <img src={getImageURL({ path: media, size: "", type: "message" })} className="bubble-img" />
-              </Link>
-            }
-          </div>
-        </div>
-      </div>
-    ) : (
-      <div className="chat-window-message">
-        <div className="chat-box-user">
-          <Link to={"/profile/" + userId}>
-            {type == "system" ?
-              <img src="/imgs/system.png" alt={name} />
-              :
-              <img src={image} alt={name} />
-            }
-          </Link>
-
-        </div>
-        <div className='text'>
-          <div className='gray fs-08 d-flex align-items-center mb-2'>
-            {general == "general" &&
-              <Link to={"/profile/" + userId}>
-                <h6 className="name me-2">{user.nickname}</h6>
-              </Link>
-
-            }
-            {type == "system" &&
-              <h6 className="name">Системное сообщение</h6>
-
-            }
-            <time className='me-2'>{time}</time>
-          </div>
-          <div className={type == "system" || !status ? "bubble colored" : "bubble"}>
+            </div>
             {status ?
-              <>
+              <div className={type == "system" ? "bubble-system" : "bubble"}>
+                {type == "system" &&
+                  <img src="/imgs/logo-mess.svg" />
+                }
                 {text && <p>{text}</p>}
                 {media &&
-                  <Link to={getImageURL({ path: media, size: "", type: "message" })}>
-                    <img src={getImageURL({ path: media, size: "", type: "message" })} className="bubble-img" />
-                  </Link>
+                  <img src={getImageURL({ path: media, size: "", type: "message" })} onClick={setShowShare} className="bubble-img" />
                 }
-              </>
+              </div>
               :
-              <p>Сообщение заблокировано администрацией</p>
+              <i className='bubble-ban'>Сообщение заблокировано администрацией Rush to Play</i>
             }
           </div>
+          {type == "system" ?
+            <img src="/imgs/system.png" alt={name} />
+            :
+            <img src={image} alt={name} />
+          }
         </div>
-      </div>
-    )
+
+      ) : (
+        admin ? (
+          <div className="chat-window-message">
+            <img src={image} alt={name} />
+            <div className='text'>
+              <div className='gray fs-08 d-flex align-items-center mb-2'>
+                <h6 className="me-2 fs-12">ADMIN </h6>
+                <time className='me-2'> {time}</time>
+
+              </div>
+              <div className="bubble-admin">
+                {text && <p>{text}</p>}
+                {media &&
+                  <img src={getImageURL({ path: media, size: "", type: "message" })} onClick={setShowShare} className="bubble-img" />
+                }
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div className="chat-window-message">
+            <div className="chat-box-user">
+              <Link to={"/profile/" + userId}>
+                {type == "system" ?
+                  <img src="/imgs/system.png" alt={name} />
+                  :
+                  <img src={image} alt={name} />
+                }
+              </Link>
+
+            </div>
+            <div className='text'>
+              <div className='gray fs-08 d-flex align-items-center mb-2'>
+                {general == "general" &&
+                  <Link to={"/profile/" + userId}>
+                    <h6 className="name me-2">{user?.nickname}</h6>
+                  </Link>
+
+                }
+                {type == "system" &&
+                  <h6 className="name me-2">Системное сообщение</h6>
+
+                }
+                <time className='me-2'>{time}</time>
+              </div>
+              {status ?
+                <div className={type == "system" ? "bubble-system" : "bubble"}>
+                  {type == "system" &&
+                    <img src="/imgs/logo-mess.svg" onClick={setShowShare} />
+                  }
+                  {text && <p>{text}</p>}
+                  {media &&
+                    <img src={getImageURL({ path: media, size: "", type: "message" })} onClick={setShowShare} className="bubble-img" />
+                  }
+                </div>
+                :
+                <i className='bubble-ban'>Сообщение заблокировано администрацией Rush to Play</i>
+              }
+            </div>
+          </div>
+        )
+      )}
+      <Modal show={showShare} onHide={setShowShare} centered size="xl" className='modal'>
+        <Modal.Header closeButton></Modal.Header>
+        <Modal.Body className='modal-dialog'>
+          <img src={getImageURL({ path: media, size: "", type: "message" })} onClick={setShowShare} className="modal-img" />
+        </Modal.Body>
+      </Modal>
+    </>
   )
 };
 export default Message;
